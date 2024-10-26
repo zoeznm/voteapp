2c7b378222b429bbf6f0dd6b1fdda58669b61c68
zoeznm <bomikim046@gmail.com>
2024-10-26 14:53:36 +0900

    Result.tsx 작성 완료

---
+++ vote-analysis-app/vote-app/src/app/pages/Results.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResultChart from '../components/ResultChart';
+
+const Results = () => {
+  return (
+    <div>
+      <h2>투표 결과</h2>
+      <ResultChart />
+    </div>
+  );
+};
+
+export default Results;

c8a4691ef99811d9ad74be0ea754bbfae54e07b2
zoeznm <bomikim046@gmail.com>
2024-10-27 12:09:35 +0900

    DataDisplay를 통해 데이터를 렌더링

--- vote-analysis-app/analysis-app/src/app/app.tsx
+++ vote-analysis-app/analysis-app/src/app/app.tsx
@@ -1,53 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+// src/app/app.tsx
+import React from 'react';
+import { useFetchVotes } from './hooks/useFetchVotes';
+import DataDisplay from './components/DataDisplay';
 
-import NxWelcome from './nx-welcome';
+const App: React.FC = () => {
+  const { votesData, loading } = useFetchVotes();
 
-import { Route, Routes, Link } from 'react-router-dom';
+  if (loading) {
+    return <p>로딩 중...</p>;
+  }
 
-export function App() {
   return (
     <div>
-      <NxWelcome title="analysis-app" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
+      <h1>Analysis App</h1>
+      <DataDisplay votesData={votesData} />
     </div>
   );
-}
+};
 
 export default App;

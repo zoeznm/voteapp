0eea8509937dc21674cb87797ff9f0a992da3ed5
zoeznm <bomikim046@gmail.com>
2024-10-27 21:51:38 +0900

    results page 수정 및 기능 추가 완료

--- vote-analysis-app/vote-app/src/app/pages/Results.tsx
+++ vote-analysis-app/vote-app/src/app/pages/Results.tsx
@@ -1,27 +1,45 @@
+// Result.tsx
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import { Chart, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, ArcElement, Tooltip, Legend } from 'chart.js';
+import styled from 'styled-components';
 
-Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend); // 필요한 스케일 및 요소 등록
+ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Tooltip, Legend);
+
+const ChartContainer = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  max-height : 600px;
+`;
 
 const Result: React.FC = () => {
-  const voteData = JSON.parse(localStorage.getItem('voteData') || '{}');
+  const storedTopics = JSON.parse(localStorage.getItem('topics') || '[]');
+  const voteData = storedTopics.length > 0 ? storedTopics[storedTopics.length - 1] : null;
 
-  const data = {
+  if (!voteData) {
+    return <p>투표 결과가 없습니다.</p>;
+  }
+
+  const barData = {
     labels: Object.keys(voteData.votes),
     datasets: [
       {
-        label: '투표 결과',
+        label: '투표 결과 (막대 그래프)',
         data: Object.values(voteData.votes),
         backgroundColor: 'rgba(75, 192, 192, 0.6)',
       },
     ],
   };
 
+
   return (
     <div>
-      <h1>{voteData.topic}</h1>
-      <Bar data={data} />
+      <h1>투표 결과</h1>
+      <ChartContainer>
+      <h2>{voteData.topic}</h2>
+        <Bar data={barData} options={{ responsive: true, maintainAspectRatio: false }} />
+      </ChartContainer>
     </div>
   );
 };

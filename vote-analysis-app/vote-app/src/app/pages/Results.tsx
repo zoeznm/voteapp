a89ecf1f5530c557f4d950856fd5efba91532872
zoeznm <bomikim046@gmail.com>
2024-10-28 18:00:26 +0900

    공통 컴포넌트 불러오기

--- vote-analysis-app/vote-app/src/app/pages/Results.tsx
+++ vote-analysis-app/vote-app/src/app/pages/Results.tsx
@@ -1,17 +1,10 @@
 // Result.tsx
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import {Chart} from '@vote-app/shared-ui';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, ArcElement, Tooltip, Legend } from 'chart.js';
-import styled from 'styled-components';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Tooltip, Legend);
 
-const ChartContainer = styled.div`
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  max-height : 600px;
-`;
 
 const Result: React.FC = () => {
   const storedTopics = JSON.parse(localStorage.getItem('topics') || '[]');
@@ -21,25 +14,11 @@ const Result: React.FC = () => {
     return <p>투표 결과가 없습니다.</p>;
   }
 
-  const barData = {
-    labels: Object.keys(voteData.votes),
-    datasets: [
-      {
-        label: '투표 결과 (막대 그래프)',
-        data: Object.values(voteData.votes),
-        backgroundColor: 'rgba(75, 192, 192, 0.6)',
-      },
-    ],
-  };
-
 
   return (
     <div>
       <h1>투표 결과</h1>
-      <ChartContainer>
-      <h2>{voteData.topic}</h2>
-        <Bar data={barData} options={{ responsive: true, maintainAspectRatio: false }} />
-      </ChartContainer>
+      <Chart type="bar" data={voteData.votes} title={voteData.topic} />
     </div>
   );
 };

d2c9015a06d2be053064b72e1f2ecba6450b25e1
zoeznm <bomikim046@gmail.com>
2024-10-29 00:50:43 +0900

    빌드 설정 해주기

--- vote-analysis-app/analysis-app/webpack.config.js
+++ vote-analysis-app/analysis-app/webpack.config.js
@@ -3,8 +3,11 @@ const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
 const { join } = require('path');
 
 module.exports = {
+  // 출력 경로 설정
   output: {
-    path: join(__dirname, '../dist/analysis-app'),
+    path: join(__dirname, './build'), // 경로를 vote-app으로 변경
+    filename: '[name].[contenthash].js', // 출력 파일 이름에 해시 추가
+    clean: true, // 이전 빌드 결과물 삭제
   },
   devServer: {
     port: 4200,
--- vote-analysis-app/vote-app/webpack.config.js
+++ vote-analysis-app/vote-app/webpack.config.js
@@ -1,10 +1,11 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
+const { NxWebpackPlugin } = require('@nx/webpack');  // NxWebpackPlugin 추가
 const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../dist/vote-app'),
+    path: join(__dirname, './build'),
+    filename: '[name].[contenthash].js',  // 빌드 시 해싱 추가
+    clean: true,  // 이전 빌드 파일 정리
   },
   devServer: {
     port: 4200,
@@ -15,21 +16,31 @@ module.exports = {
     },
   },
   plugins: [
-    new NxAppWebpackPlugin({
-      tsConfig: './tsconfig.app.json',
-      compiler: 'babel',
-      main: './src/main.tsx',
-      index: './src/index.html',
-      baseHref: '/',
-      assets: ['./src/favicon.ico', './src/assets'],
-      styles: ['./src/styles.css'],
+    new NxWebpackPlugin({
+      tsConfig: join(__dirname, './tsconfig.app.json'),
+      main: join(__dirname, './src/main.tsx'),
+      index: join(__dirname, './src/index.html'),
+      assets: [join(__dirname, './src/favicon.ico'), join(__dirname, './src/assets')],
+      styles: [join(__dirname, './src/styles.css')],
       outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
       optimization: process.env['NODE_ENV'] === 'production',
     }),
-    new NxReactWebpackPlugin({
-      // Uncomment this line if you don't want to use SVGR
-      // See: https://react-svgr.com/
-      // svgr: false
-    }),
+    // 추가된 NxWebpackPlugin 사용을 통해 최적화
   ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',  // TSX 파일에 babel-loader 사용
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],  // CSS 로더 추가
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
